e77ada5603120c5383fc984d614d39f4572f8ed7
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2025-03-21 08:24:06 +0000

    remaining features + enhance feature extraction script with logging and error handling

--- scripts/metadata_collection/feature_extraction.py
+++ scripts/metadata_collection/feature_extraction.py
@@ -2,29 +2,44 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-
 from pathlib import Path
-
+import logging
 from tsfeatures import tsfeatures
+from src.load_data.config import DATASETS
 
-from src.load_data.config import DATASETS, DATA_GROUPS, GROUP_IDX
 
-data_name, group = DATA_GROUPS[GROUP_IDX]
-print(data_name, group)
+# data_name, group = 'M3', 'Monthly'
+data_name, group = "M3", "Quarterly"
+# data_name, group = "Tourism", "Monthly"
+# data_name, group = "Tourism", "Quarterly"
+# data_name, group = "M4", "Monthly"
+# data_name, group = "M4", "Quarterly"
+# data_name, group = "M4", "Weekly"
+# data_name, group = "M4", "Daily"
+
 data_loader = DATASETS[data_name]
 
-df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(
-    group, extended=True
-)
-# df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(group, sample_n_uid=3)
+logging.basicConfig(level=logging.INFO)
+logging.info(f"Starting processing for dataset: {data_name}, group: {group}")
 
-train, _ = data_loader.train_test_split(df, horizon=horizon)
+try:
+    df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(
+        group, extended=True
+    )
+    logging.info(f"Data loaded with horizon: {horizon}, frequency: {freq_str}")
 
-if __name__ == "__main__":
-    feat_df = tsfeatures(train, freq=freq_int, threads=8)
+    train, _ = data_loader.train_test_split(df, horizon=horizon)
 
-    outfile = Path(__file__).parent.parent.parent.parent / "assets" / "features"
+    if __name__ == "__main__":
+        outfile = Path(__file__).resolve().parents[2] / "assets" / "features"
+        logging.info(f"Saving to {outfile}")
 
-    feat_df.to_csv(f"{outfile}/train_feature_set_{data_name},{group}.csv", index=False)
-    print(feat_df)
+        outfile.mkdir(parents=True, exist_ok=True)
+        feat_df = tsfeatures(train, freq=freq_int)
+
+        output_path = outfile / f"train_feature_set_{data_name},{group}.csv"
+        feat_df.to_csv(output_path, index=False)
+        logging.info(f"Features saved to {output_path}")
+        print(feat_df)
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
