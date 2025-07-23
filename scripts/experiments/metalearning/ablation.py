e45c887f594ccf7f412538302bf6779566c5ef90
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-07-23 11:18:51 +0100

    ablation

--- scripts/experiments/metalearning/ablation.py
+++ scripts/experiments/metalearning/ablation.py
@@ -57,7 +57,9 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
     mod = ClassifierChain(LGBMClassifier(verbosity=-1))
     mod_reg = RegressorChain(LGBMRegressor(verbosity=-1))
     mod_mo = MultiOutputClassifier(LGBMClassifier(verbosity=-1))
-    mod_reg_mo = RegressorChain(LGBMRegressor(verbosity=-1))
+    mod_reg_mo1 = RegressorChain(LGBMRegressor(verbosity=-1))
+    mod_reg_mo2 = XGBRFRegressor()
+    # se depois usar RF, tenho de adicionar MO e chain a ablation
 
     meta_arima = MetaARIMA(model=mod,
                            freq=freq_str,
@@ -68,7 +70,7 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
                            base_optim=BASE_OPTIM,
                            mmr_lambda=LAMBDA)
 
-    meta_arima_pca = MetaARIMA(model=mod_reg_mo,
+    meta_arima_pca = MetaARIMA(model=mod_reg_mo2,
                                freq=freq_str,
                                season_length=freq_int,
                                n_trials=N_TRIALS,
@@ -77,6 +79,16 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
                                use_mmr=True,
                                base_optim=BASE_OPTIM,
                                mmr_lambda=LAMBDA)
+
+    meta_arima_pca_chain = MetaARIMA(model=mod_reg_mo1,
+                                     freq=freq_str,
+                                     season_length=freq_int,
+                                     n_trials=N_TRIALS,
+                                     target_pca=True,
+                                     quantile_thr=QUANTILE_THR,
+                                     use_mmr=True,
+                                     base_optim=BASE_OPTIM,
+                                     mmr_lambda=LAMBDA)
 
     meta_arima_regr = MetaARIMA(model=mod_reg,
                                 freq=freq_str,
@@ -129,6 +141,8 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
     meta_arima.meta_fit(X_train, y_train)
     print('\t MetaARIMA(PCA)')
     meta_arima_pca.meta_fit(X_train, y_train)
+    print('\t MetaARIMA(PCA,Chain)')
+    meta_arima_pca_chain.meta_fit(X_train, y_train)
     print('\t MetaARIMA(R)')
     meta_arima_regr.meta_fit(X_train, y_train)
     print('\t MetaARIMA(No-SH)')
@@ -142,6 +156,7 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
 
     pred_list = meta_arima.meta_predict(X_test)
     pred_list_pca = meta_arima_pca.meta_predict(X_test)
+    pred_list_pca_ch = meta_arima_pca_chain.meta_predict(X_test)
     pred_list_regr = meta_arima_regr.meta_predict(X_test)
     pred_list_nosh = meta_arima_nosh.meta_predict(X_test)
     pred_list_mc = meta_arima_mc.meta_predict(X_test)
@@ -151,41 +166,48 @@ for j, (train_index, test_index) in enumerate(kfcv.split(X)):
     for i, (uid, x) in enumerate(X_test.iterrows()):
         print(i, uid)
 
-        df_uid = train.query(f'unique_id=="{uid}"').copy()
+        try:
+            df_uid = train.query(f'unique_id=="{uid}"').copy()
 
-        meta_arima.fit(df_uid, config_space=pred_list[i])
-        meta_arima_pca.fit(df_uid, config_space=pred_list_pca[i])
-        meta_arima_regr.fit(df_uid, config_space=pred_list_regr[i])
-        meta_arima_nosh.fit(df_uid, config_space=pred_list_nosh[i])
-        meta_arima_mc.fit(df_uid, config_space=pred_list_mc[i])
-        meta_arima_mo.fit(df_uid, config_space=pred_list_mo[i])
-        meta_arima_nommr.fit(df_uid, config_space=pred_list_nommr[i])
+            meta_arima.fit(df_uid, config_space=pred_list[i])
+            meta_arima_pca.fit(df_uid, config_space=pred_list_pca[i])
+            meta_arima_pca_chain.fit(df_uid, config_space=pred_list_pca_ch[i])
+            meta_arima_regr.fit(df_uid, config_space=pred_list_regr[i])
+            meta_arima_nosh.fit(df_uid, config_space=pred_list_nosh[i])
+            meta_arima_mc.fit(df_uid, config_space=pred_list_mc[i])
+            meta_arima_mo.fit(df_uid, config_space=pred_list_mo[i])
+            meta_arima_nommr.fit(df_uid, config_space=pred_list_nommr[i])
 
-        err_meta = cv.loc[uid, meta_arima.selected_config]
-        err_meta_pca = cv.loc[uid, meta_arima_pca.selected_config]
-        err_meta_regr = cv.loc[uid, meta_arima_regr.selected_config]
-        err_meta_nosh = cv.loc[uid, meta_arima_nosh.selected_config]
-        err_meta_mc = cv.loc[uid, meta_arima_mc.selected_config]
-        err_meta_mo = cv.loc[uid, meta_arima_mo.selected_config]
-        err_meta_nommr = cv.loc[uid, meta_arima_nommr.selected_config]
+            err_meta = cv.loc[uid, meta_arima.selected_config]
+            err_meta_pca = cv.loc[uid, meta_arima_pca.selected_config]
+            err_meta_pca_ch = cv.loc[uid, meta_arima_pca_chain.selected_config]
+            err_meta_regr = cv.loc[uid, meta_arima_regr.selected_config]
+            err_meta_nosh = cv.loc[uid, meta_arima_nosh.selected_config]
+            err_meta_mc = cv.loc[uid, meta_arima_mc.selected_config]
+            err_meta_mo = cv.loc[uid, meta_arima_mo.selected_config]
+            err_meta_nommr = cv.loc[uid, meta_arima_nommr.selected_config]
 
-        comp = {
-            'MetaARIMA': err_meta,
-            'MetaARIMA(PCA)': err_meta_pca,
-            'MetaARIMA(R)': err_meta_regr,
-            'MetaARIMA(No-SH)': err_meta_nosh,
-            'MetaARIMA(MC)': err_meta_mc,
-            'MetaARIMA(MO)': err_meta_mo,
-            'MetaARIMA(No-MMR)': err_meta_nommr,
-        }
+            comp = {
+                'MetaARIMA': err_meta,
+                'MetaARIMA(PCA)': err_meta_pca,
+                'MetaARIMA(PCA,CH)': err_meta_pca_ch,
+                'MetaARIMA(R)': err_meta_regr,
+                'MetaARIMA(No-SH)': err_meta_nosh,
+                'MetaARIMA(MC)': err_meta_mc,
+                'MetaARIMA(MO)': err_meta_mo,
+                'MetaARIMA(No-MMR)': err_meta_nommr,
+            }
 
-        pprint(comp)
+            pprint(comp)
 
-        results_df = pd.DataFrame(results)
-        print(results_df.mean())
-        print(results_df.median())
+            results_df = pd.DataFrame(results)
+            print(results_df.mean())
+            print(results_df.median())
 
-        results.append(comp)
+            results.append(comp)
+
+        except ValueError:
+            continue
 
 results_df = pd.DataFrame(results)
 results_df.to_csv(f'assets/results/sensitivity/ablation,{data_name},{group}.csv', index=False)
