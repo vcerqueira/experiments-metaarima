65bb4863a68ac2021207accc74835951e19639f2
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-09-02 11:44:42 +0100

    quick checks on results

--- scripts/experiments/analysis/results.py
+++ scripts/experiments/analysis/results.py
@@ -23,12 +23,12 @@ X, y, _, _, cv = mdr.read(fill_na_value=-1)
 
 results_df = pd.read_csv(f'{RESULTS_DIR}/{data_name},{group}.csv')
 
-res = results_df.merge(cv[['auto_config']], on='unique_id')
-
-res['d'] = res['MetaARIMA']-res['AutoARIMA']
-res = res.sort_values('d', ascending=False)
-
-res.loc[res['d'] > 0.025,:]['auto_config'].value_counts()
+# res = results_df.merge(cv[['auto_config']], on='unique_id')
+#
+# res['d'] = res['MetaARIMA']-res['AutoARIMA']
+# res = res.sort_values('d', ascending=False)
+#
+# res.loc[res['d'] > 0.025,:]['auto_config'].value_counts()
 
 print(results_df.mean(numeric_only=True))
 print(results_df.dropna().mean(numeric_only=True))
--- scripts/experiments/analysis/results2.py
+++ scripts/experiments/analysis/results2.py
@@ -45,3 +45,33 @@ print(results_df.loc[results_df['AutoARIMA2'].isna(),:].mean())
 print(results_df.loc[results_df['AutoARIMA2'].isna(),:].median())
 
 
+
+#
+
+import pandas as pd
+from plotnine import ggplot, element_text,aes, geom_bar, theme,theme_minimal, labs, coord_flip
+
+# Your data as a pandas Series
+s = results_df.drop(columns=['AutoARIMA2']).median(numeric_only=True).sort_values()
+# s = results_df.drop(columns=['AutoARIMA2']).rank(axis=1, na_option='bottom').mean().sort_values()
+
+# Convert to DataFrame for plotnine
+df = s.reset_index()
+df.columns = ['Method', 'AverageRank']
+
+# Create the barplot
+p = (
+    ggplot(df, aes(x='reorder(Method, AverageRank)', y='AverageRank'))
+    + geom_bar(stat='identity', fill='#4C72B0')
+    # + coord_flip()  # Optional: horizontal bars
+    + theme_minimal()
+    + theme(axis_text=element_text(size=12))
+    + labs(
+        x='',
+        y='SMAPE',
+        title=''
+    )
+)
+
+print(p)
+p.save('avg_smape.pdf', width=12,height=4.5)
