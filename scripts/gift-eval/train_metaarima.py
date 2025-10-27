0dafc24bd6dc05af323e20c767b8839023f1070a
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-10-27 20:24:33 +0000

    gift-eval prep

--- scripts/gift-eval/train_metaarima.py
+++ scripts/gift-eval/train_metaarima.py
@@ -3,7 +3,7 @@ import joblib
 from xgboost import XGBRFRegressor
 
 from src.meta.arima.meta_arima import MetaARIMA
-from src.meta.arima._data_reader import MetadataReader
+from src.meta.arima._data_reader import MetadataReader, ModelIO
 from src.load_data.config import DATASETS
 from src.config import (MMR,
                         N_TRIALS,
@@ -12,7 +12,7 @@ from src.config import (MMR,
                         LAMBDA,
                         PCA_N_COMPONENTS)
 
-FILENAME = 'assets/trained_metaarima_m4m.sav'
+FILENAME = 'assets/trained_metaarima_m4m.joblib.gz'
 # -- train metamodel
 source_data_name, source_group = 'M4', 'Monthly'
 data_loader = DATASETS[source_data_name]
@@ -27,17 +27,16 @@ meta_arima = MetaARIMA(model=XGBRFRegressor(),
                        n_trials=N_TRIALS,
                        quantile_thr=QUANTILE_THR,
                        # pca_n_components=PCA_N_COMPONENTS,
-                       pca_n_components=25,  # for smaller model
+                       pca_n_components=50,  # for smaller model
                        use_mmr=MMR,
                        base_optim=BASE_OPTIM,
                        mmr_lambda=LAMBDA)
 
 meta_arima.meta_fit(X, y)
-# meta_arima.meta_predict(X.head(2))
 
-joblib.dump(meta_arima, FILENAME)
+ModelIO.save_model(meta_arima, FILENAME)
 
-loaded_metaarima = joblib.load(FILENAME)
+loaded_metaarima = ModelIO.load_model(FILENAME)
 
 preds = loaded_metaarima.meta_predict(X.head(2))
 
