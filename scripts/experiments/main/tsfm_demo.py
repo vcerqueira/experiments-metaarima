6c32b73fa82841ef2812ad8068837a8be85c9119
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-05-18 11:34:16 +0100

    tsfm todos

--- scripts/experiments/main/tsfm_demo.py
+++ scripts/experiments/main/tsfm_demo.py
@@ -5,11 +5,7 @@ import pandas as pd
 
 from utilsforecast.losses import mase
 from statsforecast import StatsForecast
-from statsforecast.models import (AutoARIMA,
-                                  AutoETS,
-                                  AutoTheta,
-                                  SeasonalNaive,
-                                  ARIMA)
+from statsforecast.models import AutoARIMA, SeasonalNaive
 
 from src.meta.arima._data_reader import ModelIO
 from src.chronos_data import ChronosDataset
@@ -21,18 +17,19 @@ FILENAME = f'assets/trained_metaarima_{source}_{algorithm}.joblib.gz'
 meta_arima = ModelIO.load_model(FILENAME)
 
 target = 'monash_m1_monthly'
-# target = 'monash_hospital'
+
 df, horizon, _, freq, seas_len = ChronosDataset.load_everything(target)
 train, test = ChronosDataset.time_wise_split(df, horizon)
 
 sf_models = [AutoARIMA(season_length=seas_len),
              SeasonalNaive(season_length=seas_len)]
 
-model_names = ['MetaARIMA', 'AutoARIMA','SeasonalNaive']
+#  TODO ADD TSFM'S NAMES TO LIST
+model_names = ['MetaARIMA', 'AutoARIMA', 'SeasonalNaive']
 
 uids = train['unique_id'].unique().tolist()
 
-results = []
+results, predictions = [], []
 for uid in uids:
     print(uid)
     # uid="T000055"
@@ -49,26 +46,37 @@ for uid in uids:
     sf = StatsForecast(models=deepcopy(sf_models), freq=freq)
     sf.fit(df_uid_tr)
 
-    fcst_aa = sf.forecast(df=df_uid_tr,h=horizon)
+    fcst_aa = sf.forecast(h=horizon)
+
+    # TODO tsfm inference
+    fcst_tsfm1 = tsfm.predict(h=horizon)
+    # TODO transform to structure like fcst_aa
 
     if OVERRIDE_DS:
         fcst_ma['ds'] = df_uid_ts['ds'].values
         fcst_aa['ds'] = df_uid_ts['ds'].values
+        # TODO override values
+        fcst_tsfm1['ds'] = fcst_tsfm1['ds'].values
 
     uid_test = df_uid_ts.merge(fcst_ma, on=['unique_id', 'ds'])
     uid_test = uid_test.merge(fcst_aa, on=['unique_id', 'ds'])
+    # TODO add to uid_test
+    uid_test = uid_test.merge(fcst_tsfm1, on=['unique_id', 'ds'])
 
     err = mase(df=uid_test, models=model_names, seasonality=seas_len, train_df=df_uid_tr)
 
     pprint(err)
 
+    predictions.append(uid_test)
     results.append(err)
     results_df = pd.concat(results)
     print(results_df.mean(numeric_only=True))
     print(results_df.median(numeric_only=True))
 
 results_df = pd.concat(results)
+predictions_df = pd.concat(predictions).reset_index(drop=True)
 print(results_df.mean(numeric_only=True))
 print(results_df.median(numeric_only=True))
 
-results_df.to_csv(f'assets/results/main/{target}.csv', index=False)
+results_df.to_csv(f'assets/results/tsfm/scores,{target}.csv', index=False)
+predictions_df.to_csv(f'assets/results/tsfm/predictions,{target}.csv', index=False)
